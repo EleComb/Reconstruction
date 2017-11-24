8900d0c99a5939103e7934d972194bbbfd8ef09d
mikoto <electomb@gmail.com>
2017-11-24 23:02:51 +0800

    1.1——原始程序，设计得不太好的程序。

---
+++ src/main/java/_1/Movie.java
@@ -0,0 +1,33 @@
+package _1;
+
+/**
+ * 电影有小孩，普通，新票三种类型。
+ * 电影标题，票价，两个属性。
+ */
+public class Movie {
+
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String _title;
+    private int _priceCode;
+
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+    }
+
+    public int getPriceCode() {
+        return _priceCode;
+    }
+
+    public void setPriceCode(int arg) {
+        _priceCode = arg;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+}
---
+++ src/main/java/_1/Rental.java
@@ -0,0 +1,24 @@
+package _1;
+
+/**
+ * 顾客租了某个影片
+ * */
+public class Rental {
+
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+}
