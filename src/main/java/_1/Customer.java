55b58e4ea6043b6ffd328c32480dc317292c596d
mikoto <electomb@gmail.com>
2017-11-25 01:11:42 +0800

    找出逻辑泥团并运用Extract Method Alt+Shift+M Extract Method

--- src/main/java/_1/Customer.java
+++ src/main/java/_1/Customer.java
@@ -34,7 +34,7 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            thisAmount += amountFor(each);
+            thisAmount = amountFor(each);
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -52,24 +52,25 @@ public class Customer {
     }
 
     // 找出逻辑泥团并运用Extract Method
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
+    // Alt+Shift+M Extract Method
+    private double amountFor(Rental aRental) {
+        double result = 0;
         //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
+        switch (aRental.getMovie().getPriceCode()) {
             case Movie.REGULAR :
-                thisAmount += 2;
-                if(each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
+                result += 2;
+                if(aRental.getDaysRented() > 2)
+                    result += (aRental.getDaysRented() - 2) * 1.5;
+            break;
             case Movie.NEW_RELEASE :
-                thisAmount += each.getDaysRented() * 3;
-                break;
+                result += aRental.getDaysRented() * 3;
+            break;
             case Movie.CHILDRENS :
-                thisAmount += 1.5;
-                if(each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
+                result += 1.5;
+                if(aRental.getDaysRented() > 3)
+                    result += (aRental.getDaysRented() - 3) * 1.5;
+            break;
         }
-        return thisAmount;
+        return result;
     }
 }
