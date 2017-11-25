820619f22a3825ca99a12b9807ffa1be2eda0f26
mikoto <electomb@gmail.com>
2017-11-25 11:30:36 +0800

    最好不要再另一个对象的属性基础上运用switch语句。如果要switch也要在类自己的数据上使用。 运用多态取代与价格相关的条件逻辑

--- src/main/java/_1/Movie.java
+++ src/main/java/_1/Movie.java
@@ -30,4 +30,40 @@ public class Movie {
         return _title;
     }
 
+    /**
+     *  1. 找出逻辑泥团并运用Extract Method
+     *     Alt+Shift+M Extract Method
+     *  2. 函数应该放在它使用的数据所属对象内
+     *     Alt+Shift+V Move Method
+     * */
+    double getCharge(int daysRented) {
+        double result = 0;
+        //determine amounts for each line
+        switch (getPriceCode()) {
+            case Movie.REGULAR :
+                result += 2;
+                if(daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE :
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS :
+                result += 1.5;
+                if(daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    // 运用多态取代与价格相关的条件逻辑
+    public int getFrequentRenterPoints(int daysRented) {
+        // add bonus for a two day new release rental
+        if((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1 )
+            return 2;
+        else
+            return 1;
+    }
+
 }
--- src/main/java/_1/Rental.java
+++ src/main/java/_1/Rental.java
@@ -21,38 +21,11 @@ public class Rental {
         return _movie;
     }
 
-    /**
-     *  1. 找出逻辑泥团并运用Extract Method
-     *     Alt+Shift+M Extract Method
-     *  2. 函数应该放在它使用的数据所属对象内
-     *     Alt+Shift+V Move Method
-     * */
     double getCharge() {
-        double result = 0;
-        //determine amounts for each line
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR :
-                result += 2;
-                if(getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-            break;
-            case Movie.NEW_RELEASE :
-                result += getDaysRented() * 3;
-            break;
-            case Movie.CHILDRENS :
-                result += 1.5;
-                if(getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-            break;
-        }
-        return result;
+        return _movie.getCharge(_daysRented);
     }
 
     public int getFrequentRenterPoints() {
-        // add bonus for a two day new release rental
-        if((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1 )
-            return 2;
-        else
-            return 1;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
