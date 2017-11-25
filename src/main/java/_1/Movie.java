6d31c483b12e9831ce10e59259ed23095c407a1b
mikoto <electomb@gmail.com>
2017-11-25 13:47:50 +0800

    state模式：
     1. 使用Replace Type Code with State/Strategy，将与类型相关的行为搬移至State模式内。
                a) 针对代码使用SelfEncapsulate Field
                b) 抽象Price类。
     2. 运用Move Method将switch语句移到Price类。
    
     3. 运用Replace Conditional with Polymorphism去掉switch语句。

--- src/main/java/_1/Movie.java
+++ src/main/java/_1/Movie.java
@@ -49,24 +49,7 @@ public class Movie {
      *     Alt+Shift+V Move Method
      * */
     double getCharge(int daysRented) {
-        double result = 0;
-        //determine amounts for each line
-        switch (getPriceCode()) {
-            case Movie.REGULAR :
-                result += 2;
-                if(daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE :
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS :
-                result += 1.5;
-                if(daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     // 运用多态取代与价格相关的条件逻辑
--- src/main/java/_1/Price.java
+++ src/main/java/_1/Price.java
@@ -2,11 +2,26 @@ package _1;
 
 public abstract class Price {
     abstract int getPriceCode();
+
+    /**
+     *  1. 找出逻辑泥团并运用Extract Method
+     *     Alt+Shift+M Extract Method
+     *  2. 函数应该放在它使用的数据所属对象内
+     *     Alt+Shift+V Move Method
+     * */
+    abstract double getCharge(int daysRented);
 }
 
 class ChildrensPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDRENS;
+    }
+
+    double getCharge(int daysRented) {
+        double result = 1.5;
+        if(daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+        return result;
     }
 }
 
@@ -14,10 +29,22 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
+
 }
 
 class RegularPrice extends Price {
     int getPriceCode() {
         return Movie.REGULAR;
+    }
+
+    double getCharge(int daysRented) {
+        double result = 2;
+        if(daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
     }
 }
