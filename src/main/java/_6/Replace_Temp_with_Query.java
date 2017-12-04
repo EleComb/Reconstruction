1034453d70bb5474944f2e60bf6d810081904de1
mikoto <electomb@gmail.com>
2017-12-04 21:37:41 +0800

    Replace_Temp_with_Query以查询取代临时变量2 前

--- src/main/java/_6/Replace_Temp_with_Query.java
+++ src/main/java/_6/Replace_Temp_with_Query.java
@@ -5,7 +5,16 @@ package _6;
  * */
 public class Replace_Temp_with_Query {
 
-    double quantity = 10, itemPrice = 10;
+    int quantity = 10, itemPrice = 10;
+
+    // 将两个临时变量去掉，一次一个。
+    double getPrice() {
+        int basePrice = quantity * itemPrice;
+        double discountFactor;
+        if(basePrice > 1000) discountFactor = 0.95;
+        else discountFactor = 0.98;
+        return basePrice * discountFactor;
+    }
 
     public double test() {
         if(getBasePrice() > 1000) {
