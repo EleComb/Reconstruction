1573399eee35feddc0e0fe32befec7205b47c27b
mikoto <electomb@gmail.com>
2017-12-04 21:43:08 +0800

    提炼所有变量！

--- src/main/java/_6/Replace_Temp_with_Query.java
+++ src/main/java/_6/Replace_Temp_with_Query.java
@@ -10,12 +10,17 @@ public class Replace_Temp_with_Query {
     /**
      * 将两个临时变量去掉，一次一个。
      * 先声明final确保变量只赋值一次。
+     * 再一个一个变成声明式将他们去除。
      */
     double getPrice() {
+        return getBasePrice() * getDiscountFactor();
+    }
+
+    private double getDiscountFactor() {
         final double discountFactor;
         if(getBasePrice() > 1000) discountFactor = 0.95;
         else discountFactor = 0.98;
-        return getBasePrice() * discountFactor;
+        return discountFactor;
     }
 
     public double test() {
