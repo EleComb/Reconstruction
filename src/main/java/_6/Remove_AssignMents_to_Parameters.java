f8266588ded6f34c020d7766ee99f609a9664f45
mikoto <electomb@gmail.com>
2017-12-05 12:43:24 +0800

    Remove Assignments to Parameters移除对参数的赋值 后

--- src/main/java/_6/Remove_AssignMents_to_Parameters.java
+++ src/main/java/_6/Remove_AssignMents_to_Parameters.java
@@ -5,9 +5,12 @@ package _6;
  * */
 public class Remove_AssignMents_to_Parameters {
 
-    public int discount(int inputVal, int quantity, int yearToDate){
-        if(inputVal > 50) inputVal -= -2;
-        return 0;
+    public int discount(final int inputVal,final int quantity,final int yearToDate){
+        int result = inputVal;
+        if(inputVal > 50) result -= -2;
+        if(quantity > 100) result -= 1;
+        if(yearToDate > 10000) result -= 4;
+        return result;
     }
 
 }
