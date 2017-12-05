af9610a3c71c9856e8d5d08577d399641ddf2061
mikoto <electomb@gmail.com>
2017-12-05 10:55:40 +0800

    no message

--- src/main/java/_6/Introduce_Explaining_Variable.java
+++ src/main/java/_6/Introduce_Explaining_Variable.java
@@ -1,4 +1,24 @@
 package _6;
 
+/**
+ * 引入解释性变量
+ * */
 public class Introduce_Explaining_Variable {
+
+    public void test() {
+        String platform = "MAC", browser = "IE";
+        int resize = 1;
+
+        final boolean isMacOs = platform.toUpperCase().indexOf("MAC") > -1;
+        final boolean isIEBrowser = browser.toUpperCase().indexOf("IE") > -1;
+        final boolean wasResized = resize > 0;
+        if( isMacOs && isIEBrowser &&
+            wasInitialized() && wasResized) {
+            System.out.println(1);
+        }
+    }
+
+    private boolean wasInitialized() {
+        return true;
+    }
 }
