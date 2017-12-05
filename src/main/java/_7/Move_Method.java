883791bf7b8b3178ebe5266146775284eca66add
EleComb <ele_comb@qq.com>
2017-12-05 15:37:13 +0800

    重构后1

--- src/main/java/_7/Move_Method.java
+++ src/main/java/_7/Move_Method.java
@@ -6,26 +6,38 @@ public class Move_Method {
         AccountType type;
         int daysOverdrawn = 10;
 
-        double overdraftCharge() {
-            if(type.isPremium()) {
-                double result = 10;
-                if(daysOverdrawn > 7) result += (daysOverdrawn - 7) * 0.85;
-                return result;
-            } else {
-                return daysOverdrawn * 1.75;
-            }
-        }
+//        double overdraftCharge() {
+//            return type.overdraftCharge(daysOverdrawn);
+//        }
 
         double bankCharge() {
             double result = 4.5;
-            if(daysOverdrawn > 0) result += overdraftCharge();
+//            if(daysOverdrawn > 0) result += overdraftCharge();
+            if(daysOverdrawn > 0) result += type.overdraftCharge(daysOverdrawn);
             return result;
+        }
+
+        double getDaysOverDrawn() {
+            return 0;
         }
     }
 
     class AccountType {
         boolean isPremium() {
             return false;
+        }
+
+        /**
+         * 这个情况就要搬回去
+         * */
+        double overdraftCharge(Account account) {
+            if(isPremium()) {
+                double result = 10;
+                if(account.getDaysOverDrawn() > 7) result += (account.getDaysOverDrawn() - 7) * 0.85;
+                return result;
+            } else {
+                return account.getDaysOverDrawn() * 1.75;
+            }
         }
     }
 }
