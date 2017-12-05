120e3313c8cacf03e1050b24961e9c6ecf3ef06c
EleComb <ele_comb@qq.com>
2017-12-05 16:37:20 +0800

    Extract Class提炼类

---
+++ src/main/java/_7/Extract_Class.java
@@ -0,0 +1,38 @@
+package _7;
+
+public class Extract_Class {
+    class Person {
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getOfficeAreaCode() {
+            return officeAreaCode;
+        }
+
+        public void setOfficeAreaCode(String officeAreaCode) {
+            this.officeAreaCode = officeAreaCode;
+        }
+
+        public String getOfficeNumber() {
+            return officeNumber;
+        }
+
+        public void setOfficeNumber(String officeNumber) {
+            this.officeNumber = officeNumber;
+        }
+
+        private String name;
+        private String officeAreaCode;
+        private String officeNumber;
+    }
+
+    class TelephoneNumber{
+        private TelephoneNumber officeTelephone = new TelephoneNumber();
+    }
+
+}
