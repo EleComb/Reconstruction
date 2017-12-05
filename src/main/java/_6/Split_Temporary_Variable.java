1b2b7d00adc0f11d016bdeb1fc2f8ce04e34c89b
mikoto <electomb@gmail.com>
2017-12-05 11:36:07 +0800

    范例2

--- src/main/java/_6/Split_Temporary_Variable.java
+++ src/main/java/_6/Split_Temporary_Variable.java
@@ -7,10 +7,28 @@ public class Split_Temporary_Variable {
 
     private double height = 1, width = 2;
 
+    private double primaryForce = 3, mass = 4, secondaryForce = 5;
+    int delay = 6;
+
     public void test() {
         final double perimeter = 2 * (height * width);
         System.out.println(perimeter);
         final double area = height * width;
         System.out.println(area);
+    }
+
+    //范例2
+    double getDistanceTravelled(int time) {
+        double result;
+        double acc = primaryForce / mass;
+        int primaryTime = Math.min(time, delay);
+        result = 0.5 * acc * primaryTime * primaryTime;
+        int secondaryTime = time - delay;
+        if(secondaryTime > 0) {
+            double primaryVel = acc * delay;
+            acc = (primaryForce + secondaryForce) / mass;
+            result += primaryVel + secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+        }
+        return result;
     }
 }
