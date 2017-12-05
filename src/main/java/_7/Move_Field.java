2871ea181cfc1bbbbb9c439bf879bea7d5b4af54
EleComb <ele_comb@qq.com>
2017-12-05 16:19:06 +0800

    重构后

--- src/main/java/_7/Move_Field.java
+++ src/main/java/_7/Move_Field.java
@@ -6,15 +6,24 @@ package _7;
 public class Move_Field {
     class Account {
         private AccountType type;
-        private double interestRate;
 
         double interestForAmount_days(double amount, int days) {
-            return interestRate * amount * days / 365;
+            return type.getInterestRate() * amount * days / 365;
         }
     }
 
     class AccountType {
-        
+
+        public double getInterestRate() {
+            return interestRate;
+        }
+
+        public void setInterestRate(double interestRate) {
+            this.interestRate = interestRate;
+        }
+
+        private double interestRate;
+
         boolean isPremium() {
             return false;
         }
