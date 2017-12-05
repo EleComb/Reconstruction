a1653c419f0c61bba78b9209520f633cd6bd43a5
mikoto <electomb@gmail.com>
2017-12-05 14:29:50 +0800

    重构后

--- src/main/java/_6/Substitute_Algorithm.java
+++ src/main/java/_6/Substitute_Algorithm.java
@@ -1,22 +1,30 @@
 package _6;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 替换算法
  * */
 public class Substitute_Algorithm {
 
     String foundPerson(String[] people) {
+        List candidates = Arrays.asList(new String[]{"Don", "John", "Kent"});
         for (int i = 0; i < people.length; i++) {
-            if(people[i].equals("Don")) {
-                return "Don";
-            }
-            if(people[i].equals("John")) {
-                return "John";
-            }
-            if(people[i].equals("Kent")) {
-                return "Kent";
-            }
+            if(candidates.contains(people[i]))
+                return people[i];
         }
+//        for (int i = 0; i < people.length; i++) {
+//            if(people[i].equals("Don")) {
+//                return "Don";
+//            }
+//            if(people[i].equals("John")) {
+//                return "John";
+//            }
+//            if(people[i].equals("Kent")) {
+//                return "Kent";
+//            }
+//        }
         return "";
     }
 
