7c172fdb5d668d2cb9de1cfe0e91b4c7bbab2f04
EleComb <ele_comb@qq.com>
2017-12-07 23:58:36 +0800

    _1_Self_Encapsulate_Field　a

--- src/main/java/_8/_1_Self_Encapsulate_Field.java
+++ src/main/java/_8/_1_Self_Encapsulate_Field.java
@@ -6,6 +6,16 @@ package _8;
 public class _1_Self_Encapsulate_Field {
 
     private int low;
+    private int high;
+
+    private void initialize(int low, int high) {
+        this.low = low;
+        this.high = high;
+    }
+
+    _1_Self_Encapsulate_Field(int low, int high) {
+        initialize(low, high);
+    }
 
     public int getLow() {
         return low;
@@ -23,9 +33,28 @@ public class _1_Self_Encapsulate_Field {
         this.high = high;
     }
 
-    private int high;
+
 
     boolean inchudes(int arg) {
         return arg >= getLow() && arg <= getHigh();
+    }
+
+}
+
+class CappedRage extends _1_Self_Encapsulate_Field {
+
+    private int cap;
+
+    public int getCap() {
+        return cap;
+    }
+
+    public void setCap(int cap) {
+        this.cap = cap;
+    }
+
+    CappedRage(int low, int high, int cap) {
+        super(low, high);
+        this.cap = cap;
     }
 }
