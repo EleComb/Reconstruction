40d247d0d567dc0d452625156b661777a2460c41
EleComb <ele_comb@qq.com>
2017-12-08 00:16:31 +0800

    _2_Replace_Data_Value_with_Object   b

---
+++ src/main/java/_8/_2_Replace_Data_Value_with_Object.java
@@ -0,0 +1,35 @@
+package _8;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class _2_Replace_Data_Value_with_Object {
+
+    private static int numberOfOrdersFor(Collection orders, String customer) {
+        int result = 0;
+        Iterator iter = orders.iterator();
+        while (iter.hasNext()) {
+            Order each = (Order) iter.next();
+            if(each.getCustomer().equals(customer)) result++;
+        }
+    }
+
+    class Order {
+
+        private String customer;
+
+        public Order(String customer) {
+            this.customer = customer;
+        }
+
+        public String getCustomer() {
+            return customer;
+        }
+
+        public void setCustomer(String customer) {
+            this.customer = customer;
+        }
+
+    }
+
+}
