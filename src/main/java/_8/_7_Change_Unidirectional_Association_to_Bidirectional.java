67cc0d3d92a78857747991f165801c5da1d3007b
EleComb <ele_comb@qq.com>
2017-12-16 11:07:33 +0800

    _7_Change_Unidirectional_Association_to_Bidirectional a (怪怪的

--- src/main/java/_8/_7_Change_Unidirectional_Association_to_Bidirectional.java
+++ src/main/java/_8/_7_Change_Unidirectional_Association_to_Bidirectional.java
@@ -23,6 +23,16 @@ public class _7_Change_Unidirectional_Association_to_Bidirectional {
             if(this.customer != null)
                 this.customer.friendOrders().add(this);
         }
+
+        void addCustomer(Customer customer) {
+            customer.friendOrders().add(this);
+            this.customer.friendOrders().add(customer);
+        }
+
+        void removeCustomer(Customer customer) {
+            customer.friendOrders().remove(this);
+            this.customer.friendOrders().remove(customer);
+        }
     }
 
     class Customer {
@@ -31,6 +41,14 @@ public class _7_Change_Unidirectional_Association_to_Bidirectional {
         Set friendOrders() {
             /** should only be used by Order when modifying the association */ 
             return order;
+        }
+
+        void addOrder(Order order) {
+            order.addCustomer(this);
+        }
+
+        void removeCustomer(Order order) {
+            order.removeCustomer(this);
         }
     }
 }
