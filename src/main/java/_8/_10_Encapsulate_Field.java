0baeb9036a22ae342e27a00c54d00fe8bd7b26f8
EleComb <ele_comb@qq.com>
2017-12-16 13:42:09 +0800

    _9_Replace_Magic_Number_with_Symbilic_Costant b

---
+++ src/main/java/_8/_10_Encapsulate_Field.java
@@ -0,0 +1,4 @@
+package _8;
+
+public class _10_Encapsulate_Field {
+}
---
+++ src/main/java/_8/_9_Replace_Magic_Number_with_Symbilic_Costant.java
@@ -0,0 +1,11 @@
+package _8;
+
+/**
+ * 以字面常量取代魔法数
+ * */
+public class _9_Replace_Magic_Number_with_Symbilic_Costant {
+
+    double potentialEnergy(double mass, double height) {
+        return mass * 9.81 * height;
+    }
+}
