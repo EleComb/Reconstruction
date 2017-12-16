7f98557947b3a648089e84504fd5a7dbb3fe008b
EleComb <ele_comb@qq.com>
2017-12-16 15:37:03 +0800

    _13_Replace_Type_Code_with_Class a

--- src/main/java/_8/_13_Replace_Type_Code_with_Class.java
+++ src/main/java/_8/_13_Replace_Type_Code_with_Class.java
@@ -3,22 +3,48 @@ package _8;
 public class _13_Replace_Type_Code_with_Class {
 
     class Person {
-        public static final int O = 0;
-        public static final int A = 1;
-        public static final int B = 2;
-        public static final int AB = 3;
+//        public final int O = BloodGroup.O.getCode();
+//        public final int A = BloodGroup.A.getCode();
+//        public final int B = BloodGroup.B.getCode();
+//        public final int AB = BloodGroup.AB.getCode();
 
         private int bloodGroup;
 
-        public Person(int booldGroup) {
-            this.bloodGroup = booldGroup;
-        }
+//        public Person(int booldGroup) {
+//            this.bloodGroup = booldGroup;
+//        }
+//
+//        public void setBloodGroup(int bloodGroup) {
+//            this.bloodGroup = bloodGroup;
+//        }
+//
+//        public int getBloodGroupCode() {
+//            return bloodGroup;
+//        }
+    }
 
-        public void setBloodGroup(int bloodGroup) {
+    static class BloodGroup {
+        public static final BloodGroup O = new BloodGroup(BloodGroup.O);
+        public static final BloodGroup A = new BloodGroup(BloodGroup.A);
+        public static final BloodGroup B = new BloodGroup(BloodGroup.B);
+        public static final BloodGroup AB = new BloodGroup(BloodGroup.AB);
+        private static final BloodGroup[] values = {O, A, B, AB};
+
+        private BloodGroup bloodGroup;
+
+        public BloodGroup(BloodGroup bloodGroup) {
             this.bloodGroup = bloodGroup;
         }
 
-        public int getBloodGroup() {
+        public static BloodGroup code(int arg) {
+            return values[arg];
+        }
+
+        private void setBloodGroup(BloodGroup bloodGroup) {
+            this.bloodGroup = bloodGroup;
+        }
+
+        private BloodGroup getBloodGroup() {
             return bloodGroup;
         }
     }
